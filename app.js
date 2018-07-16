aa0147336e084065034be3c5825f7d2eb36f1581
kellandrosu <kellandr@oregonstate.edu>
2018-07-16 11:27:15 -0700

    dfs partially implimented

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require('express-ws')(app);
 const scraper = require('./scraper.js');
 
 //pass in callback function to handle links
-scraper.scrape('http://andriuskelly.com', function(links){console.log(links);});
+scraper.crawl('http://andriuskelly.com', function(links){console.log(links);});
 
 
 app.set('port', process.argv[2]);
--- scraper.js
+++ scraper.js
@@ -11,19 +11,26 @@ function WebLink(url){
 
 //scraper API
 module.exports = {
+
 	scrape: function(url, responseCall){
 		console.log("scraping: " + url);
 		scrape(url, "", responseCall);
-	}
-	crawl: function(qry){
+	},
+	crawl: function(qry, serverFunc){
 		//placeholder:
-		qry = {url: "https://en.wikipedia.org/wiki/Main_Page", searchMethod: "DFS", stopKeyword: "Oregon", size: "10"};
-		crawl(qry);
+		qry = {	
+				url: "https://en.wikipedia.org/wiki/Main_Page", 
+				searchMethod: "DFS", 
+				stopKeyword: "Oregon", 
+				size: "10"
+			};
+
+		crawl(qry, serverFunc);
 	}
 };
 
 //TODO: impliment BFS, DFS
-function crawl(qry){
+function crawl(qry, serverFunc){
 
 	//crawled urls to prevent loops
 	var visited = [];
@@ -35,7 +42,10 @@ function crawl(qry){
 
 	const method = qry['searchMethod'].toLowerCase();
 
+	//recursive helper
 	crawlHelper(rootNode, method, depth, visited);
+
+	serverFunc(rootNode);
 }
 
 
@@ -43,7 +53,9 @@ function crawlHelper(node, method, depth, visited){
 	
 	if( depth <= 0 ) { return; }
 	
-	scrape(node.url, keyword, function( links ){
+	//pass dfs/bfs function to scraper to perform crawl
+	scrape(node.url, function( links ){
+
 		//in bfs, add new node for each link
 		if ( method == 'bfs'){
 			for (link in links){
@@ -56,10 +68,9 @@ function crawlHelper(node, method, depth, visited){
 		}	
 		//in dfs, randomly choose link
 		else {
-
 			var index = Math.floor( Math.random() * links.length() );
-
-			while( links.length > 0 links[index] in visited )
+			//make sure not visited
+			while( links.length > 0 && links[index] in visited ){
 				links.splice(index, 1);
 				index = Math.floor( Math.random() * links.length() )
 			}
@@ -70,14 +81,16 @@ function crawlHelper(node, method, depth, visited){
 
 				const childNode = new WebLink( links[index] ) 
 				node.webLinks.push( childNode );
-				crawlHelper(childNode, method, depth-1 visited)
+				crawlHelper(childNode, method, depth-1, visited);
 			}
 		}
 	});
 }
 
 //webscraping reference: https://codeburst.io/an-introduction-to-web-scraping-with-node-js-1045b55c63f7
-function scrape(url, keyword, callback){
+//scrapes url site and performs callback on each link
+function scrape(url, callback){
+	
 	//options for request
 	const options = {		
 	  uri: url,
@@ -91,14 +104,18 @@ function scrape(url, keyword, callback){
 	//calls request, executes promise
 	rp(options)
 	  .then(($) => {
+
 //TODO: impliment keyword stop
+
 	    $('a').each( function(i, e){
+	    	
 	    	var link = $(this).attr('href')
-	    	//if link is self
+	    	
+	    	//if link address is self-referenced, insert domain
 	    	if( link.substring(0,2) == './' ){
 	    		link = url + link.substring(1);
 	    	}
-
+	    	//add unique link
 	    	if( link.startsWith('http') && !links.includes(link)){
 	    		links.push(link);
 	    	}
