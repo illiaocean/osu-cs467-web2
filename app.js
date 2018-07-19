2582a71d5a979520b4370af66ba9bd21992c563c
illia-osu <abdullai@oregonstate.edu>
2018-07-20 00:02:37 +0300

    Initial graph implementation.

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
 const express = require('express');
 const app = express();
 const scraper = require('./scraper.js');
+require('express-ws')(app);
 
 
 //placeholder query:
-qry = { 
-        url: 'https://en.wikipedia.org/wiki/Special:Random', 
-        searchMethod: "dfs", 
-        stopKeyword: "Oregon", 
-        size: "7"
-    };
+qry = {
+    url: 'https://en.wikipedia.org/wiki/Special:Random',
+    searchMethod: "dfs",
+    stopKeyword: "Oregon",
+    size: "7"
+};
 
-scraper.crawl(qry, function(node){
+scraper.crawl(qry, function (node) {
 
     //this function will be executed on the node tree result of the dfs/bfs
     //each node is structured:
-        //node.url is the url of the node
-        //node.webLinks is an array of child nodes
+    //node.url is the url of the node
+    //node.webLinks is an array of child nodes
+
+    console.log(JSON.stringify(node));
 
     //example: this will print dfs results 
-    while(node){
+    while (node) {
         console.log(node.url);
         node = node.webLinks[0];
     }
 });
 
-require('express-ws')(app);
-
 app.set('port', process.argv[2]);
 app.use(express.static(__dirname + '/public'));
 
 //client-server communication via websockets
-app.ws('/', function(ws) {
-    ws.on('message', function(msg) {
+app.ws('/', function (ws) {
+    ws.on('message', function (msg) {
         console.log("Server received message", msg);
         msg = JSON.parse(msg);
 
@@ -44,8 +45,13 @@ app.ws('/', function(ws) {
             ws.send(JSON.stringify({code: "searching"}));
 
             //begin search
-            scraper.scrape(msg.data, function(links){
-                console.log(links);
+            scraper.crawl(msg.data, function (node) {
+                //Search has finished. Send results back to the client.
+                var response = {
+                    code: "results",
+                    data: node
+                };
+                ws.send(JSON.stringify(response));
             });
         }
     });
--- scraper.js
+++ scraper.js
@@ -1,177 +1,180 @@
-
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 
 
 //constructor for WebLink object
-function WebLink(url){
-		this.url = url;
-		this.webLinks = [];
+function WebLink(url) {
+    this.url = url;
+    this.webLinks = [];
 }
 
 //scraper API
 module.exports = {
+    scrape: function (url, callback) {
+        console.log("scraping: " + url);
+        scrape(url, callback);
+    },
+    crawl: function (qry, serverFunc) {
 
-	scrape: function(url, callback){
-		console.log("scraping: " + url);
-		scrape(url, callback);
-	},
-	crawl: function(qry, serverFunc){
-
-		crawl(qry, serverFunc);
-	}
+        crawl(qry, serverFunc);
+    }
 };
 
-//TODO: impliment BFS, DFS
-function crawl(qry, serverFunc){
 
-	//crawled urls to prevent loops
-	var visited = [];
-	visited.push( qry['url'] );
+function crawl(qry, serverFunc) {
+    //crawled urls to prevent loops
+    var visited = [];
+    visited.push(qry['url']);
+    var depth = parseInt(qry['size']);
+    var rootNode = new WebLink(qry['url']);
+    var method = qry['searchMethod'].toLowerCase();
 
-	var depth = parseInt( qry['size'] );
-
-	var rootNode = new WebLink( qry['url']  )
-
-	if ( qry['searchMethod'].toLowerCase() == 'bfs'){
-		bfs(rootNode, depth, function(){
-			serverFunc(rootNode);
-		});
-	}
-	else if ( qry['searchMethod'].toLowerCase() == 'dfs' ) {
-		dfs(rootNode, depth, visited, function(){
-			serverFunc(rootNode);
-		});
-	}
-	else {
-		console.log("scraper.js: Could not parse search method.")
-		serverFunc(null);
-	}
+    if (method === 'bfs') {
+        bfs(rootNode, depth, function () {
+            serverFunc(rootNode);
+        });
+    } else if (method === 'dfs') {
+        dfs(rootNode, depth, visited, function () {
+            serverFunc(rootNode);
+        });
+    } else {
+        console.log("scraper.js: Could not parse search method.")
+        serverFunc(null);
+    }
 }
 
 
+function bfs(root, depth, callback) {
+    var queue = new Queue();
+    var visited = [];
 
-function bfs(root, depth, callback){
+    queue.enqueue(root);
+    var nodesInLayer = 1;
 
-	var queue = new Queue();
-	var visited = [];
+    while (depth > 0 && !queue.isEmpty()) {
 
-	queue.enqueue(root);
-	var nodesInLayer = 1;
+        --nodesInLayer;
 
-	while( depth > 0 && !queue.isEmpty() ) {
+        if (nodesInLayer <= 0) {
+            --depth;
+            console.log("depth:" + depth);
+            nodesInLayer = queue.getLength();
+        }
 
-		--nodesInLayer;
+        var node = queue.dequeue();
+        visited.push(node.url);
 
-		if(nodesInLayer <= 0){
-			--depth;
-			console.log("depth:" + depth);
-			nodesInLayer = queue.getLength();
-		}
+        scrape(node.url, function (links) {
 
-		var node = queue.dequeue();
-		visited.push(node.url);
+            for (link in links) {
+                if (!(link in visited)) {
+                    var childNode = new WebLink(link);
 
-		scrape(node.url, function(links){
+                    queue.enqueue(childNode);
+                    node.webLinks.push(childNode);
+                }
+            }
+        });
+    }
 
-			for (link in links){
-				if( !(link in visited) ){
-					var childNode = new WebLink(link)
-
-					queue.enqueue(childNode);
-					node.webLinks.push( childNode );
-				}
-			}
-		});
-	}
-	callback();
+    callback();
 }
 
 
+function dfs(node, depth, visited, callback) {
+    if (depth <= 0) {
+        callback();
+        return;
+    }
 
+    //pass dfs/bfs function to scraper to perform crawl
+    scrape(node.url, function (links) {
+        console.log(node.url);
+        console.log("links: " + links.length + " depth: " + depth);
 
-function dfs(node, depth, visited, callback){
+        //in dfs, randomly choose link
+        var index = Math.floor(Math.random() * links.length);
 
-	if( depth <= 0 ) { 
-		callback();
-		return; 
-	}
-	
-	//pass dfs/bfs function to scraper to perform crawl
-	scrape(node.url, function( links ){
+        //make sure not visited
+        while (links.length > 0 && links[index] in visited) {
+            links.splice(index, 1);
+            index = Math.floor(Math.random() * links.length)
+        }
 
-		console.log(node.url); 
-		console.log("links: " + links.length + " depth: " + depth);
+        if (links.length > 0) {
+            visited.push(links[index]);
 
-		//in dfs, randomly choose link 
-		var index = Math.floor( Math.random() * links.length );
-		//make sure not visited
-		while( links.length > 0 && links[index] in visited ){
-			links.splice(index, 1);
-			index = Math.floor( Math.random() * links.length )
-		}
+            const childNode = new WebLink(links[index])
+            node.webLinks.push(childNode);
 
-		if( links.length > 0 ){ 
-			
-			visited.push( links[index] );
-
-			const childNode = new WebLink( links[index] ) 
-			node.webLinks.push( childNode );
-
-			dfs(childNode, depth-1, visited, callback);
-		}
-		else {
-			//TODO: handle dead end
-			return;
-		}		
-	});
+            dfs(childNode, depth - 1, visited, callback);
+        } else {
+            //TODO: handle dead end
+        }
+    });
 }
 
 
 //webscraping reference: https://codeburst.io/an-introduction-to-web-scraping-with-node-js-1045b55c63f7
 //scrapes url site and performs callback on each link
-function scrape(url, callback){
+function scrape(url, callback) {
 
-	//options for request
-	const options = {		
-	  uri: url,
-	  transform: function (body) {
-	    return cheerio.load(body);
-	  }
-	};
-  	
-  	var links = [];
-  	var link = '';
+    //options for request
+    const options = {
+        uri: url,
+        transform: function (body) {
+            return cheerio.load(body);
+        }
+    };
 
-	//calls request, executes promise
-	rp(options)
-	  .then(($) => {
+    var links = [];
+    var link = '';
 
-//TODO: impliment keyword stop
+    //calls request, executes promise
+    rp(options).then(($) => {
+        //TODO: implement keyword stop
+        $('a').each(function () {
+            if (link = $(this).attr('href')) {
+                //if link address is self-referenced, insert domain
+                if (link.substring(0, 2) == './') {
+                    link = url + link.substring(1);
+                }
 
-	    $('a').each( function(){
-	    	
-	    	if ( link = $(this).attr('href') ) {
-
-		    	//if link address is self-referenced, insert domain
-		    	if( link.substring(0,2) == './' ){
-		    		link = url + link.substring(1);
-		    	}
-		    	//add unique link
-		    	if( link.startsWith('http') && !links.includes(link)){
-		    		links.push(link);
-		    	}
-		    }
-	    });
-    	callback(links)
-	  })
-	  .catch((err) => {
-	    console.log(err);
-	  });
+                //add unique link
+                if (link.startsWith('http') && !links.includes(link)) {
+                    links.push(link);
+                }
+            }
+        });
+        callback(links)
+    }).catch((err) => {
+        console.log(err);
+    });
 
     return links;
 }
 
 
-//Queue implimentation without using shift() from http://code.iamkate.com/javascript/queues/
-function Queue(){var a=[],b=0;this.getLength=function(){return a.length-b};this.isEmpty=function(){return 0==a.length};this.enqueue=function(b){a.push(b)};this.dequeue=function(){if(0!=a.length){var c=a[b];2*++b>=a.length&&(a=a.slice(b),b=0);return c}};this.peek=function(){return 0<a.length?a[b]:void 0}};
+//Queue implementation without using shift() from http://code.iamkate.com/javascript/queues/
+function Queue() {
+    var a = [], b = 0;
+    this.getLength = function () {
+        return a.length - b
+    };
+    this.isEmpty = function () {
+        return 0 == a.length
+    };
+    this.enqueue = function (b) {
+        a.push(b)
+    };
+    this.dequeue = function () {
+        if (0 != a.length) {
+            var c = a[b];
+            2 * ++b >= a.length && (a = a.slice(b), b = 0);
+            return c
+        }
+    };
+    this.peek = function () {
+        return 0 < a.length ? a[b] : void 0
+    }
+};
